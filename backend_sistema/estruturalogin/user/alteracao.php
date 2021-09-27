10b0065b0c0411ee82c9d21969f02cc2d5129ff1
Pablo Henrique Vieira de Nadai <pablo.nadai@etec.sp.gov.br>
2021-09-27 14:50:56 -0300

    V:1.4 - Tela de Alteração

--- backend_sistema/estruturalogin/user/alteracao.php
+++ backend_sistema/estruturalogin/user/alteracao.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
     <meta charset="UTF-8">
-    <title> Alterar Dados de Vacina </title>
+    <head>
+        <title> Alterar Dados de Vacina </title>
+        <link href="/TCC-Projeto-Equipe/assets/mainEstilos/styleAlterar.css" rel="stylesheet">
+    </head>
     <body>
     <?php
         session_start();
@@ -9,12 +12,22 @@
             header("Location: ../index.php");
         }
     ?>
-        <h3>Alterar Dados de Vacina</h3>
-        <form name="nome" action="veralteracao.php" method="post">
-            <b>Código da Vacina:</b> <input type="number" name="ID_vacina"><br><br>
-            <input type="submit" value="Ok">
-            <input type="reset" value="Limpar">
-            <input type='button' onclick="window.location = 'index.php';" value="Voltar">
-        </form>
+        <div class="content">
+            <div class="info">
+                <h3 class="title">Alterar Dados de Vacina</h3>
+                <form name="nome" action="veralteracao.php" method="post" class="form">
+                    Insira o código da vacina que você deseja alterar:
+                    <label class="icon-input">
+                            <i class="fas fa-address-book icon-mdy"></i>
+                            <input type="number" name="ID_vacina" placeholder="Exemplo: 2">
+                    </label>   
+                    <div class="botoes">
+                        <button class="btn btn-style2" type="submit" value="Ok">Alterar</button>
+                        <button class="btn btn-style2" type="reset" value="Limpar">Limpar</button>
+                        <button class="btn btn-style2" onclick="window.location = 'index.php';" value="Voltar">Voltar</button> 
+                    </div>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
