be6448dffb2b6911eaef5dc3aeb9915a34b1ba97
Thaissa da Silva Ferrari <thaissa.ferrari@etec.sp.gov.br>
2021-10-05 07:37:27 -0300

    V:1.7 - Estilização da tela excluir

--- backend_sistema/estruturalogin/user/exclusao.php
+++ backend_sistema/estruturalogin/user/exclusao.php
@@ -1,6 +1,8 @@
 <html>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title> Excluir Vacinas </title>
+    <link href="../../../assets/mainEstilos/styleExclusao.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d1fdd19268.js" crossorigin="anonymous"></script>
     <body>
     <?php
         session_start();
@@ -8,12 +10,22 @@
             header("Location: ../index.php");
         }
     ?>
-        <h3> Exclusão de Vacinas</h3>
-        <form name="nome" action="verexclusao.php" method="post">
-            <b>Código da vacina:</b> <input type="number" name="ID_vacina"><br><br>
-            <input type="submit" value="Ok">
-            <input type="reset" value="Limpar">
-            <input type='button' onclick="window.location = 'index.php';" value="Voltar">
-        </form>
+    <div class="content">
+        <div class="info">
+            <h3> Exclusão de Vacinas</h3>
+            <form name="nome" action="verexclusao.php" method="post" class="form">
+                    Código da Vacina:
+                    <label class="icon-input">
+                    <i class="fas fa-file-signature icon-mdy"></i>
+                    <input type="number" name="ID_vacina" placeholder="..."><br><br>
+                    </label>
+                <div class="botoes">
+                    <button button class="btn btn-style2" type="submit" value="Ok">Ok</button>
+                    <button class="btn btn-style2" type="reset" value="Limpar">Limpar</button>
+                    <button class="btn btn-style2" type='button' onclick="window.location = 'index.php';" value="Voltar">Voltar</button>
+                </div>
+            </form>
+        </div>
+    </div>
     </body>
 </html>
